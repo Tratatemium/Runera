091e3452a90e48b8691f0022691ab19286f78db0
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-24 20:31:07 +0100

    Feature: implement UserEdit component with form handling and styling

--- src/pages/user/UserEdit.tsx
+++ src/pages/user/UserEdit.tsx
@@ -1,9 +1,81 @@
 import styles from "./UserEdit.module.css";
 
+import { useState } from "react";
+import { useForm } from "../../hooks/useForm";
+
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import { Button } from "../../components/Button";
+import { FormField } from "../../components/FormField";
+
+import { inputFields } from "../../config/inputFields";
+
+const userFields = [inputFields.username, inputFields.email] as const;
+
 function UserEdit() {
-    return (
-        <div></div>
-    )
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formError, setFormError] = useState<string | undefined>(undefined);
+
+  type UserEditForm = {
+    [K in (typeof userFields)[number]["id"]]: string;
+  };
+
+  const {
+    formData,
+    inputErrors,
+    setServerErrors,
+    handleChange,
+    handleInputBlur,
+    handleSubmit,
+  } = useForm<UserEditForm>(userFields);
+
+  function submitUserEdit() {}
+
+  function onSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    handleSubmit(e, submitUserEdit);
+  }
+
+  return (
+    <div className={styles.page}>
+      <Header />
+      <main className={styles.main}>
+        <form className={styles.form} onSubmit={onSubmit} noValidate>
+          {userFields.map((field) => (
+            <FormField
+              key={field.id}
+              id={field.id}
+              label={field.label}
+              type={field.type}
+              placeholder={field.placeholder}
+              value={formData[field.id]}
+              onChange={handleChange}
+              onBlur={handleInputBlur}
+              inputError={inputErrors[field.id]}
+            />
+          ))}
+
+          <div className={styles.submitWrapper}>
+            {formError && (
+              <div role="alert" className={styles.errorWrapper}>
+                <p className={styles.errorText}>{formError}</p>
+              </div>
+            )}
+            <div
+              className={`${styles.submit} ${formError ? styles.error : ""}`}
+            >
+              <Button
+                buttonText="Save changes"
+                type="submit"
+                variant="primary"
+                isSubmitting={isSubmitting}
+              />
+            </div>
+          </div>
+        </form>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export {UserEdit}
+export { UserEdit };
