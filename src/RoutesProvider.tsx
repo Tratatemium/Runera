ef68832cbc548792ce1d94f54814d7541d3d3d51
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-25 14:57:09 +0100

    Feature: restructure RoutesProvider to wrap routes in PageLayout component

--- src/RoutesProvider.tsx
+++ src/RoutesProvider.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
+import { PageLayout } from "./components/PageLayout";
+import { RequireAuth } from "./components/RequireAuth";
+
 import { NotFound } from "./pages/NotFound";
 import { Home } from "./pages/Home";
 import { Signup } from "./pages/Signup";
 import { Login } from "./pages/Login";
-import { RequireAuth } from "./components/RequireAuth";
+
 import { Dashboard } from "./pages/user/Dashboard";
 import { UserInfo } from "./pages/user/UserInfo";
 import { EditAccount } from "./pages/user/EditAccount";
@@ -13,14 +16,16 @@ import { EditProfile } from "./pages/user/EditProfile";
 function RoutesProvider() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/user" element={<RequireAuth />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="info" element={<UserInfo />} />
-        <Route path="edit-account" element={<EditAccount />} />
-        <Route path="edit-profile" element={<EditProfile />} />
+      <Route element={<PageLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<RequireAuth />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="info" element={<UserInfo />} />
+          <Route path="edit-account" element={<EditAccount />} />
+          <Route path="edit-profile" element={<EditProfile />} />
+        </Route>
       </Route>
 
       <Route path="*" element={<NotFound />} />
