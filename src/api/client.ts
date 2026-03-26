495f484b0722a881bd2257748f3e307862320e59
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-26 19:27:53 +0100

    Refactor API client: remove unused error handling logic and streamline response data processing

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-import type { ErrorData, ParsedServerError } from "../types/error.types";
+import type { ApiResponse } from "../types/api.types";
 
 import { config } from "../config/appConfig";
 import {
@@ -6,46 +6,44 @@ import {
   getResponseData,
   handleServerErrors,
 } from "../utils/api.utils";
-import { ServerError } from "./errors";
 
 async function apiRequest<T>(path: string, options: RequestInit): Promise<T> {
   const response = await fetch(joinUrl(config.BASE_URL, path), {
     credentials: "include",
     ...options,
   });
-  const data = await getResponseData(response);
-  if (!response.ok) handleServerErrors(response, data as ErrorData, path);
+  const data = (await getResponseData(response)) as ApiResponse;
+  if (!response.ok) handleServerErrors(response, data);
 
   return (data as { data: T }).data;
 }
 
-function parseServerError(err: unknown): ParsedServerError {
-  if (err instanceof ServerError) {
-    const field = err.data?.error?.field;
-    const message = err.data?.error?.message;
+// function parseServerError(err: unknown): ParsedServerError {
+//   if (err instanceof ServerError) {
+//     const field = err.data?.error?.field;
+//     const message = err.data?.error?.message;
 
-    if (err.status === 409 && field) {
-      return {
-        fieldErrors: {
-          [field]: `This ${field} already exists`,
-        },
-      };
-    }
+//     if (err.status === 409 && field) {
+//       return {
+//         fieldErrors: {
+//           [field]: `This ${field} already exists`,
+//         },
+//       };
+//     }
 
-    if (err.status === 401) {
-      return {
-        generalError: "Invalid combination of login and password.",
-      };
-    }
+//     if (err.status === 401) {
+//       return {
+//         generalError: "Invalid combination of login and password.",
+//       };
+//     }
 
-    return {
-      generalError: message || "Something went wrong",
-    };
-  }
+//     return {
+//       generalError: message || "Something went wrong",
+//     };
+//   }
 
-  return {
-    generalError: "Unknown error",
-  };
-}
+//   return {
+//     generalError: "Unknown error",
+//   };
 
-export { apiRequest, ServerError, parseServerError };
+export { apiRequest };
