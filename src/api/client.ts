5511dd68a7720d529cc93ba7f936fe3cbb1a3022
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-26 22:44:31 +0100

    Refactor apiRequest function: update return type to Promise<T | null> and streamline error handling

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,13 +7,18 @@ import {
   handleServerErrors,
 } from "../utils/api.utils";
 
-async function apiRequest<T>(path: string, options: RequestInit): Promise<T> {
+async function apiRequest<T>(
+  path: string,
+  options: RequestInit,
+): Promise<T | null> {
   const response = await fetch(joinUrl(config.BASE_URL, path), {
     credentials: "include",
     ...options,
   });
-  const data = (await getResponseData(response)) as ApiResponse;
-  if (!response.ok) handleServerErrors(response, data);
+  const data = await getResponseData(response);
+  if (!response.ok) handleServerErrors(response, data as ApiResponse);
+
+  if (!data) return null;
 
   return (data as { data: T }).data;
 }
