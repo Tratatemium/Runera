a6fd3b6f3d13f73217469adf85d481db219a5bd1
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-31 14:22:01 +0200

    Feature/remove unused weather display and related styles

--- src/pages/user/MyRuns.tsx
+++ src/pages/user/MyRuns.tsx
@@ -47,22 +47,9 @@ function MyRuns() {
   //     const runId = "eaab0c97-1317-4fc8-8123-4b6f63552f72";
   //     deleteRun(runId);
   //   }
-  const weather: NonNullable<Run["weather"]>[] = [
-    "sunny",
-    "partly_cloudy",
-    "cloudy",
-    "rain",
-    "snow",
-    "windy",
-    "hot",
-    "cold",
-  ];
 
   return loading !== "fetchingRuns" ? (
     <main className={styles.main}>
-      <div className={styles.weather}>
-        {weather.map((el) => getWeatherIcon(el))}
-      </div>
       <div className={styles.runsWrapper}>
         {runsArray.map((run) => (
           <RunItem run={run} key={run.runId} />
