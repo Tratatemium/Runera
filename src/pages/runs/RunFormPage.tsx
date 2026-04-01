2a3e87d05fe8d4342341f3d33e7bda2406ca4b0f
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-04-01 14:39:11 +0200

    Feature: enhance RunFormPage with form state management and input handlers for improved user experience

--- src/pages/runs/RunFormPage.tsx
+++ src/pages/runs/RunFormPage.tsx
@@ -3,6 +3,9 @@ import styles from "./RunFormPage.module.css";
 import { inputFields } from "../../config/inputFields";
 
 import { Button, ButtonLink, FormField, Panel } from "../../components/ui/";
+import { useFormState } from "../../hooks/form/useFormState";
+import { useFormHandlers } from "../../hooks/form/useFormHandlers";
+import { useRuns } from "../../hooks/useRuns";
 
 const runFields = [
   inputFields.title,
@@ -31,17 +34,131 @@ const fieldOptionsMap = Object.fromEntries(
 );
 
 function RunFormPage() {
+  type RunForm = {
+    [K in (typeof runFields)[number]["id"]]: string;
+  };
+
+  const formStateHook = useFormState(runFields);
+  const { formState } = formStateHook;
+  const { inputHandlers, handleSubmit } = useFormHandlers(
+    runFields,
+    formStateHook,
+  );
+
+  const { loading, formError, postNewRun, updateRun } = useRuns();
+
   return (
     <main className={styles.main}>
       <Panel variant="frosted">
         <form className={styles.form} onSubmit={() => {}} noValidate>
           <FormField
             {...fieldOptionsMap.title}
-            value=""
-            onChange={()=>{}}
-            // value={formState[fieldOptionsMap.id].value}
-            // {...inputHandlers}
+            value={formState[fieldOptionsMap.title.id].value}
+            {...inputHandlers}
           />
+
+          <FormField
+            {...fieldOptionsMap.distanceKm}
+            value={formState[fieldOptionsMap.distanceKm.id].value}
+            {...inputHandlers}
+          />
+
+          <div className={styles.durationWrapper}>
+            <span className={styles.durationLabel}>Duration *</span>
+            <FormField
+              {...fieldOptionsMap.durationH}
+              value={formState[fieldOptionsMap.durationH.id].value}
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.durationM}
+              value={formState[fieldOptionsMap.durationM.id].value}
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.durationS}
+              value={formState[fieldOptionsMap.durationS.id].value}
+              {...inputHandlers}
+            />
+          </div>
+
+          <div className={styles.averagePace}>Pace</div>
+
+          <FormField
+            {...fieldOptionsMap.startTime}
+            value={formState[fieldOptionsMap.startTime.id].value}
+            {...inputHandlers}
+          />
+
+          <div className={styles.weatherWrapper}>
+            <FormField
+              {...fieldOptionsMap.weatherSunny}
+              value="sunny"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherPartlyCloudy}
+              value="partly_cloudy"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherCloudy}
+              value="cloudy"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherRain}
+              value="rain"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherSnow}
+              value="snow"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherWindy}
+              value="windy"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherHot}
+              value="hot"
+              {...inputHandlers}
+            />
+            <FormField
+              {...fieldOptionsMap.weatherCold}
+              value="cold"
+              {...inputHandlers}
+            />
+          </div>
+
+          <FormField
+            {...fieldOptionsMap.perceivedEffort}
+            value={formState[fieldOptionsMap.perceivedEffort.id].value}
+            {...inputHandlers}
+          />
+
+          <FormField
+            {...fieldOptionsMap.notes}
+            value={formState[fieldOptionsMap.notes.id].value}
+            {...inputHandlers}
+          />
+
+          <div className={`${styles.submit} ${formError ? styles.error : ""}`}>
+            <Button
+              buttonText="Save Run"
+              type="submit"
+              variant="primary"
+              isSubmitting={loading !== "idle"}
+            />
+            <ButtonLink
+              linkDirection="."
+              linkText="Cancel"
+              variant="secondary"
+              goBack={true}
+            />
+          </div>
         </form>
       </Panel>
     </main>
